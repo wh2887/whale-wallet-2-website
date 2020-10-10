0ce676ac63d35ef307b867fddf8f80daf444e60f
McCallWang <13567272790@163.com>
2020-10-10 23:29:09 +0800

    deploy

---
+++ precache-manifest.2ec229814fb0c57c9036c0c39262549b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc44551f270daa1195e",
+    "url": "/whale-wallet-2-website/css/app.42cf5dc0.css"
+  },
+  {
+    "revision": "0253ce930affd4b41f80",
+    "url": "/whale-wallet-2-website/css/npm.ant-design-vue.1b8a72bc.css"
+  },
+  {
+    "revision": "8b3886bfd9f0adb7dcbfb5f8cc8519ee",
+    "url": "/whale-wallet-2-website/img/qrCode.8b3886bf.png"
+  },
+  {
+    "revision": "8b3886bfd9f0adb7dcbfb5f8cc8519ee",
+    "url": "/whale-wallet-2-website/img/qrCode.png"
+  },
+  {
+    "revision": "b19782cedebd651724dc7ccd3109679a",
+    "url": "/whale-wallet-2-website/index.html"
+  },
+  {
+    "revision": "1bc44551f270daa1195e",
+    "url": "/whale-wallet-2-website/js/app.afe3d9a4.js"
+  },
+  {
+    "revision": "0253ce930affd4b41f80",
+    "url": "/whale-wallet-2-website/js/npm.ant-design-vue.42a5a29b.js"
+  },
+  {
+    "revision": "cca9b6c4d77edafb2be9",
+    "url": "/whale-wallet-2-website/js/npm.ant-design.38bb50e4.js"
+  },
+  {
+    "revision": "4091dd70d023e7ecb276",
+    "url": "/whale-wallet-2-website/js/npm.babel-runtime.df9410a3.js"
+  },
+  {
+    "revision": "1df9bcd3c3dd13c9483c",
+    "url": "/whale-wallet-2-website/js/npm.core-js.7e08b3cf.js"
+  },
+  {
+    "revision": "9a478cc3cca33d6fdd59",
+    "url": "/whale-wallet-2-website/js/npm.dom-align.504c077d.js"
+  },
+  {
+    "revision": "780b9f6617b381d7b1e8",
+    "url": "/whale-wallet-2-website/js/npm.echarts.3881b3dd.js"
+  },
+  {
+    "revision": "71cdedebebacd00becd5",
+    "url": "/whale-wallet-2-website/js/npm.lodash.d501822b.js"
+  },
+  {
+    "revision": "31e12c549e69f3d23dad",
+    "url": "/whale-wallet-2-website/js/npm.moment.1eff2fa8.js"
+  },
+  {
+    "revision": "1ede9d215a628fdcec6e",
+    "url": "/whale-wallet-2-website/js/npm.tinycolor2.bee92b57.js"
+  },
+  {
+    "revision": "073041ca3b74ebb9b7fc",
+    "url": "/whale-wallet-2-website/js/npm.vue-router.77a16612.js"
+  },
+  {
+    "revision": "7ed67d9775e8700d3f85",
+    "url": "/whale-wallet-2-website/js/npm.vue.8100887f.js"
+  },
+  {
+    "revision": "ffd2e7b07f6308f06b1f",
+    "url": "/whale-wallet-2-website/js/npm.vuex.efa72e44.js"
+  },
+  {
+    "revision": "88b25923f4763c5dfe5c",
+    "url": "/whale-wallet-2-website/js/npm.zrender.5f2a23fc.js"
+  },
+  {
+    "revision": "0911d55a49710f7ebca4",
+    "url": "/whale-wallet-2-website/js/runtime.18e6aa2e.js"
+  },
+  {
+    "revision": "af32b2227430505c6700",
+    "url": "/whale-wallet-2-website/js/vendors~app.6a69e1e4.js"
+  },
+  {
+    "revision": "74da3fbfc157a6f7ed4ebc1a1d52c435",
+    "url": "/whale-wallet-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/whale-wallet-2-website/robots.txt"
+  }
+]);
